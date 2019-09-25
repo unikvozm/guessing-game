9434f17b5681e797a8c44626767a84d9eb9a47a1
Natalia Volkova <natalia.volkofff@gmail.com>
2019-09-25 15:12:46 +0500

    feat: solution of the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this. min) / 2);
     }
 
     lower() {
-
+        this.max = Math.round((this.max + this. min) / 2);
     }
 
     greater() {
-
+        this.min = Math.round((this.max + this. min) / 2);
     }
 }
 
